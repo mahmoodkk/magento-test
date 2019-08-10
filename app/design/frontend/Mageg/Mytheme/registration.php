f000cf2e65fc922724b4871ee9c8de63bf133190
Mahmood <mahmood.rehman240@gmail.com>
2019-08-10 14:34:18 +0500

    Add files via upload

--- app/design/frontend/Mageg/Mytheme/registration.php
+++ app/design/frontend/Mageg/Mytheme/registration.php
@@ -5,6 +5,6 @@
 */
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::THEME,
-    'frontend/Mageg/mytheme',
+     'frontend/Mageg/luma_child',
     __DIR__
 );
